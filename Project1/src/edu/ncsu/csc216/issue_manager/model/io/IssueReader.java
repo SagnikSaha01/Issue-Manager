6593cbe8c1df4546b1aab57ae88f6bfacb5ba531
Sagnik Saha <ssaha8@ncsu.edu>
2024-02-22 01:12:01 -0500

    JavaDocs for IssueReader & IssueWriter

--- Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueReader.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueReader.java
@@ -3,13 +3,24 @@ package edu.ncsu.csc216.issue_manager.model.io;
 import java.util.ArrayList;
 
 import edu.ncsu.csc216.issue_manager.model.issue.Issue;
-
+/**
+ * Class for reading Issues from a file
+ */
 public class IssueReader {
+	/**
+	 * Reads the issues from a file and stores them in an array list
+	 * @param fileName file from which the issues will be read from
+	 * @return array list with the stored issues
+	 */
 	public static ArrayList<Issue> readIssuesFromFile(String fileName){
 		
 		return null;
 	}
-	
+	/**
+	 * Processes the issues from the file
+	 * @param fileName file from which issues will be processed
+	 * @return the issue that is processed
+	 */
     private static Issue processIssue(String fileName) {
     	return null;
     }
--- Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueWriter.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/io/IssueWriter.java
@@ -1,5 +1,18 @@
 package edu.ncsu.csc216.issue_manager.model.io;
 
-public class IssueWriter {
+import java.util.ArrayList;
 
+import edu.ncsu.csc216.issue_manager.model.issue.Issue;
+/**
+ * Class for writing Issues to a file
+ */
+public class IssueWriter {
+	/**
+	 * Writes a list of issues and saves it onto a file
+	 * @param fileName name of the file that stores the data
+	 * @param issueList list of the issues that will be saved
+	 */
+	public static void writeIssuesToFile(String fileName, ArrayList<Issue>issueList) {
+		
+	}
 }
