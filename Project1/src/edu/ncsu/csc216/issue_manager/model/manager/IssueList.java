8426f14425b90d8e9e1fa0157e621d53a0c49bb6
Sagnik Saha <ssaha8@ncsu.edu>
2024-02-22 01:31:55 -0500

    Added methods for IssueList

--- Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueList.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueList.java
@@ -1,5 +1,38 @@
 package edu.ncsu.csc216.issue_manager.model.manager;
 
-public class IssueList {
+import java.util.ArrayList;
 
+import edu.ncsu.csc216.issue_manager.model.command.Command;
+import edu.ncsu.csc216.issue_manager.model.issue.Issue;
+import edu.ncsu.csc216.issue_manager.model.issue.Issue.IssueType;
+
+public class IssueList {
+	private int counter;
+	
+	public IssueList() {
+		
+	}
+	
+	public int addIssue(IssueType issueType,String summary,String note) {
+		return 0;
+	}
+	
+	public void addIssues(ArrayList<Issue> issues) {
+		
+	}
+	private void addIssue() {
+		
+	}
+	public ArrayList<Issue> getIssues(){
+		return null;
+	}
+	public Issue getIssueById(int index) {
+		return null;
+	}
+	public void executeCommand(int index, Command c) {
+		
+	}
+	public void deleteIssueById(int index) {
+		
+	}
 }
