4f6682c2b841f2a7091bae6df11f4ca1836a300d
Sagnik Saha <ssaha8@ncsu.edu>
2024-02-22 16:56:14 -0500

    IssueReader tests

--- Project1/test/edu/ncsu/csc216/issue_manager/model/io/IssueReaderTest.java
+++ Project1/test/edu/ncsu/csc216/issue_manager/model/io/IssueReaderTest.java
@@ -1,5 +1,17 @@
 package edu.ncsu.csc216.issue_manager.model.io;
 
-public class IssueReaderTest {
+import java.util.ArrayList;
 
+import edu.ncsu.csc216.issue_manager.model.issue.Issue;
+
+public class IssueReaderTest {
+	@Test
+	public void testReadIssuesFromFile(){
+		fail();
+	}
+	@Test
+    private void tesProcessIssue() {
+    	fail();
+    }
+}
 }
