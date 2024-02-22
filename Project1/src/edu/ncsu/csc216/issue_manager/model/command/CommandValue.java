9a25f23b672d42da24c69d0248989c4e30fbbbd7
Sagnik Saha <ssaha8@ncsu.edu>
2024-02-21 23:34:19 -0500

    Adding all files back

---
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/command/CommandValue.java
@@ -0,0 +1,5 @@
+package edu.ncsu.csc216.issue_manager.model.command;
+/**
+ * Represents the constant values possible for Commands
+ */
+public enum CommandValue { ASSIGN, CONFIRM, RESOLVE, VERIFY, REOPEN }
