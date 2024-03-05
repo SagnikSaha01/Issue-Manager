14c3c80afb0c48e1f4ce3d1f273363bf5502b093
Sagnik Saha <ssaha8@ncsu.edu>
2024-03-05 13:40:31 -0500

    Updated New State transitions

--- Project1/src/edu/ncsu/csc216/issue_manager/model/issue/Issue.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/issue/Issue.java
@@ -452,11 +452,15 @@ public class Issue {
 					setConfirmed(true);
 					addNote(c.getNote());
 				} else if(c.getCommand().equals(CommandValue.RESOLVE)) {
+					if(c.getResolution().equals(Resolution.WORKSFORME) || c.getResolution().equals(Resolution.FIXED)) {
+						throw new UnsupportedOperationException("Invalid information."); 
+					} else {
 					setState(CLOSED_NAME);
 					addNote(c.getNote());
 					resolution = c.getResolution();
+					}
 				} else {
-					throw new UnsupportedOperationException("Invalid information."); 
+					throw new UnsupportedOperationException("Invalid information.");
 				}
 			}
 			/**
