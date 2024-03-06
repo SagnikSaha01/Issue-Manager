7d390b32b2a46dc377d4030a8a3f8668f6556c47
Sagnik Saha <ssaha8@ncsu.edu>
2024-03-06 11:29:52 -0500

    Creating a new issue list when loading from files

--- Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueManager.java
+++ Project1/src/edu/ncsu/csc216/issue_manager/model/manager/IssueManager.java
@@ -27,6 +27,7 @@ public class IssueManager {
 	 */
 	public void loadIssuesFromFile(String fileName) {
 		try {
+			createNewIssueList();
 			issueList.addIssues(IssueReader.readIssuesFromFile(fileName));
 		} catch (Exception e) {
 			throw new IllegalArgumentException("Invalid input.");
